448eeb046e405748923e2d792464b64b713d7abf
mujtaba.a <mujtaba.a@mytechnology-team.com>
2022-08-29 18:25:48 +0500

    Header footer brand bread

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './assets/scss/custom.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Brand from './components/Brand';
+import Login from './components/Login';
+import Bread from './components/Bread';
 
 function App() {
   return (
     <div className="my-app">
       <Header />
+      <Bread />
+      <Login />
       <Brand />
+
       <Footer />
     </div>
 
---
+++ src/components/Bread.jsx
@@ -0,0 +1,25 @@
+const Bread = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="col-lg-12">
+                    <div className="bread">
+                        <ul className="bread-ul d-flex align-items-center gx-60">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" className="active">Create  Account</a>
+
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Bread;
